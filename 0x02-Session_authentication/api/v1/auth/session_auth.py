819de4ff73b1f36eea5dd05ee22b1dfe0cc6bff5
richardrapando <122984478+richardrapando@users.noreply.github.com>
2024-04-18 09:14:40 +0300

    Session authentication

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -28,7 +28,7 @@ class SessionAuth(Auth):
             return self.user_id_by_session_id.get(session_id)
 
     def current_user(self, request=None) -> User:
-        """Retrieves user based on cookie value.
+        """Retrieves user associated with the request
         """
         user_id = self.user_id_for_session_id(self.session_cookie(request))
         return User.get(user_id)
